e93f01ee5219df67e4dc9aa74e7ea1b4f04d5d09
Alireza Sheikholmolouki <alireza.sheikholmolouki@gmail.com>
2020-07-30 15:44:59 +0430

    feat(menu): menu component added

---
+++ src/components/HeaderView/Menu.vue
@@ -0,0 +1,43 @@
+<template>
+  <ul class="menu">
+    <li class="menu__item" v-for="(item, index) of items" :key="index">{{ item.title }}</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  computed: {
+    items() {
+      return [
+        { title: 'File' },
+        { title: 'Edit' },
+        { title: 'Atoms' },
+        { title: 'View' },
+        { title: 'Help' },
+      ];
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.menu {
+  display: flex;
+  height: 100%;
+  align-items: stretch;
+  justify-content: center;
+  font-size: 0.8rem;
+  padding: 0 10px;
+
+  &__item {
+    display: flex;
+    align-items: center;
+    padding: 0 0.7em;
+    cursor: pointer;
+
+    &:hover {
+      background: $grey-5;
+    }
+  }
+}
+</style>
