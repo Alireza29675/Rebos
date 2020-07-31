f6a90ae87d3adacb34dc67de04ac59e2a956fd0a
Alireza Sheikholmolouki <alireza.sheikholmolouki@gmail.com>
2020-07-31 16:00:07 +0430

    feat(icon): boxicons added to app

---
+++ src/views/shared/Icon.vue
@@ -0,0 +1,23 @@
+<template>
+  <i :class="['bx', 'bx-xs', `bx-${className}`]"></i>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+  },
+  computed: {
+    className() {
+      return this.name.toLowerCase();
+    },
+  },
+};
+</script>
+
+<style>
+
+</style>
