363cbdcf3239a0ed96132f3ef939c82b3d9e5e6a
Alireza Sheikholmolouki <alireza.sheikholmolouki@gmail.com>
2020-07-31 16:01:37 +0430

    refactor(editor): AtomEditor moved to directory

---
+++ src/views/Home/AtomEditor/index.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="AtomEditor">
+    <EditorOperations />
+    <MonacoEditor
+      class="AtomEditor__editor"
+      theme="vs-dark"
+      v-model="code"
+      :options="options"
+      language="javascript" />
+  </div>
+</template>
+
+<script>
+import MonacoEditor from 'vue-monaco';
+import EditorOperations from './EditorOperations.vue';
+
+export default {
+  components: {
+    EditorOperations,
+    MonacoEditor,
+  },
+  data() {
+    return {
+      code: '',
+    };
+  },
+  computed: {
+    options() {
+      return {
+        fontSize: 16,
+      };
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.AtomEditor {
+  height: 100%;
+  overflow: hidden;
+  position: relative;
+
+  &__editor {
+    width: 100%;
+    height: 100%;
+  }
+}
+</style>
